19fa2ae24fdff5defa507f55a11cb02499888556
Marko <crashxx@gmail.com>
2019-03-16 01:31:27 +0100

    Add cors & route for resolving pkgname

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-const axios = require('axios')
+const express = require('express')
+const cors = require('cors')
+
+const whitelist = ['http://example1.com', 'http://example2.com']
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+
+const app = express()
+const bodyParser = require('body-parser')
 const logger = require('morgan')
+const pkginfo = require('@ma.vu/pkginfo')
+
+// app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 app.use(logger('combined'))
@@ -12,10 +28,24 @@ app.use(
   }),
 )
 
-app.get('*', (req, res) => res.send('.'))
+// app.get('*', (req, res) => res.send('.'))
 
-app.post('/info', async function(req, res) {
-  res.send('success')
+app.get('/info/:name', async (req, res) => {
+  const options = [
+    'homepage',
+    'repository',
+    'latest',
+    'name',
+    'description',
+    'version',
+    'author',
+    'license',
+    'devDependencies',
+    'dependencies',
+  ]
+  const name = req.params.name
+  const result = await pkginfo(name, options)
+  res.json(result)
 })
 
 module.exports = app
