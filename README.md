4d6331298e3dcfef08dea868070cca21d837c3b2
Marko <crashxx@gmail.com>
2019-03-24 22:05:19 +0100

    Remove files related to server

--- README.md
+++
@@ -1,7 +0,0 @@
-## What is this?
-
-Small node service using `pkginfo` module to resolve packages info
-
-### Example
-
-[https://pkginfo.herokuapp.com/info/react](https://pkginfo.herokuapp.com/info/react)
--- start.js
+++
@@ -1,9 +0,0 @@
-const app = require('./index')
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ path: 'variables.env' })
-}
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, function() {
-  console.log(`Pkginfo is listening on ${PORT}!`)
-})
